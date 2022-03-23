179f521bd1d726114ddb2f7490836d144b76bc76
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-03-23 21:28:23 +0000

    Adding Libs.

---
+++ public/js/libs/utils.js
@@ -0,0 +1,13 @@
+define(function(){
+
+    var Utils = {};
+    Utils.popupCenter = function(url, width, height, name) {
+       var left = (screen.width/2)-(width/2);
+       var top = (screen.height/2)-(height/2);
+
+       return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+   };
+
+   return Utils;
+
+});
